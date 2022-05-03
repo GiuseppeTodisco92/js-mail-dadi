6161497228dc539d95e3780f95a8f18ac09d7d2c
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-03 15:46:31 +0100

    add fix

--- giocodadi/js/main.js
+++ giocodadi/js/main.js
@@ -1,7 +1,7 @@
 // generare un numero random da 1 a 6 per il giocatore 1 
 // ed il computer , stabilire quali  di questi è il vincitore
 
-const dado = [] ;
+const dice = [] ;
 const btn = document.querySelector(".btn");
 const dadop = document.querySelector(".dado_player");
 const dadopc = document.querySelector(".dado_computer");
@@ -10,24 +10,29 @@ btn.addEventListener("click",
 function (){
 
     for (let i=0; i<6 ; i++){
-        dado[i] = Math.floor(Math.random()*6)+1
-        console.log(dado[i]);
+        dice[i] = Math.floor(Math.random()*6)+1
+        console.log(dice[i]);
     }
     
-    const player = dado[0];
-    dadop.innerText = dado[0];
-    const computer = dado[1];
-    dadopc.innerText = dado[1];
+    const player = dice[0];
+    dadop.innerText = dice[0];
+    const computer = dice[1];
+    dadopc.innerText = dice[1];
     
     console.log("player",player);
     console.log("pc",computer);
     
     if (player === computer ) {
         console.log("nessun vincitore rilancia il dado");
+        document.getElementById("result").innerHTML="nessun vincitore rilancia il dado"
     } else if (player > computer)  {
         console.log("il vincitore è il player");
+        document.getElementById("result").innerHTML="il vincitore è il player"
+
     } else {
-        console.log("il vinciotre è il computer")
+        console.log("il vincitore è il computer")
+        document.getElementById("result").innerHTML="il vincitore è il computer"
+
     }  
 }
 
